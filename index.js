b404c7f8f53ee6d3e2f8b8337d86d9f01c31b8ce
@maruf45 <armaruf450@gmail.com>
2022-10-26 23:19:08 +0600

    added blog json to server

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const cors = require("cors");
 app.use(cors());
 
 // require blogs api
-const blogs = require('blogs.json');
-
+const blogs = require("./data/blogs.json");
 
 app.get("/", (req, res) => {
   res.send(`server is running on ${port} port`);
+});
+
+app.get("/blogs", (req, res) => {
+  res.send(blogs);
 });
 
 app.listen(port, () => {
