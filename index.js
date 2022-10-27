7fc6c447760edd919ee01494182f66310b2c92d4
@maruf45 <armaruf450@gmail.com>
2022-10-27 18:23:06 +0600

    update data

--- index.js
+++ index.js
@@ -8,12 +8,35 @@ app.use(cors());
 // require blogs api
 const blogs = require("./data/blogs.json");
 
+// require categories api
+const categories = require("./data/categories.json");
+const course = require("./data/course.json");
 app.get("/", (req, res) => {
   res.send(`server is running on ${port} port`);
 });
 
 app.get("/blogs", (req, res) => {
   res.send(blogs);
+});
+
+app.get("/categories", (req, res) => {
+  res.send(categories);
+});
+
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "7") {
+    res.send(course);
+  } else {
+    const selectedCategory = course.filter((c) => c.category_id === id);
+    res.send(selectedCategory);
+  }
+});
+
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = course.find((c) => c.id === id);
+  res.send(selectedCourse);
 });
 
 app.listen(port, () => {
